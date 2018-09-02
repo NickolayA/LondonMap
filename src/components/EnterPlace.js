4bae6546b3d04aa3b7658142ad92f50a1aba7f8e
vipz3 <vipz3@mail.ru>
2018-09-03 00:24:39 +0300

    EnterPlace prototype is created

---
+++ src/components/EnterPlace.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "./EnterPlace.css";
+import { connect } from "react-redux";
+import { addNeededPlaces } from "../actions/actions";
+
+class EnterPlace extends Component {
+  state = {
+    placeName: ""
+  };
+
+  _onSubmit = () => {
+    if (this.state.placeName) {
+      this.props.addPlacesToStore(this.state.placeName);
+    } else alert("Enter place name or address");
+  };
+
+  _onChange = e => {
+    this.setState({
+      placeName: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="EnterPlace">
+        <form action="#" onSubmit={this._onSubmit}>
+          <input
+            type="text"
+            onChange={this._onChange}
+            placeholder="Enter place name or address"
+            value={this.state.placeName}
+            className={this.props.resultOfResponse}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const resultOfResponse = "error" in state.neededPlaces ? "error" : "good";
+
+  return { resultOfResponse };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addPlacesToStore: placeName => {
+      return dispatch(addNeededPlaces(placeName));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EnterPlace);
